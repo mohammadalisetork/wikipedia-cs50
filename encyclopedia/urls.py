79b8e0a2c8389fe9f9fe712ac449249ca10ede3c
mohammadali setork <mohammadalisetork@gmail.com>
2022-03-30 15:23:29 +0430

    markdown learned

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/css" , views.css , name='css')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from cgitb import html
 from django.shortcuts import render
+import markdown as md
 
 from . import util
 
@@ -7,4 +9,11 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+def css(request):
+    md.markdownFromFile(
+    input='entries/CSS.md',
+    output='encyclopedia/templates/encyclopedia/output.html',
+    encoding='utf8'
+    )
+    return render(request , 'encyclopedia/output.html')
 
